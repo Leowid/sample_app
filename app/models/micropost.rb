52d884c43c5c8b5844ce58947096a4b0ad1fe122
Leo <leo@quotespire.com>
2012-11-25 14:22:07 -0800

    new user model

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -10,8 +10,8 @@ class Micropost < ActiveRecord::Base
 
   def self.from_users_followed_by(user)
   	followed_user_ids = "SELECT followed_id FROM relationships
-  						 WHERE follower_id = :user_id"
+  						           WHERE follower_id = :user_id"
   	where("user_id IN (#{followed_user_ids}) OR user_id = :user_id", 
-  	user_id: user)
+  	user_id: user.id)
   end
 end
